f4032da93ed40ed6d803256d81a1fdbc98f8c004
kgullikson88 <kevin.gullikson@gmail.com>
2014-03-04 08:58:18 -0600

    Made a bunch of changes...

---
+++ FindSignificance.py
@@ -0,0 +1,12 @@
+import numpy
+import matplotlib.pyplot as plt
+import Bootstrap
+import sys
+
+
+if __name__ == "__main__":
+  fileList = []
+  for arg in sys.argv[1:]:
+    fileList.append(arg)
+
+  for fname in fileList:
