8d64e86a0d93b24a3a3c5a7877787c6ca09665ca
kgullikson88 <kevin.gullikson@gmail.com>
2014-05-15 09:50:59 -0500

    Added a function to plot a sample of the CCFs generated by Search*.py. It is useful for identifying companions (you should still go in with gnuplot later and figure out the right T/logg/[Fe/H] values). I also fixed a bug in SensitivityAnalysis where the vsini variable that I intended for broadening the models was being edited later in the code to smooth the simulated observations. They now have different variables so there is no conflict

---
+++ CheckCCFs.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy
+import os
+import HelperFunctions
+
+
+
+if __name__ == "__main__":
+  filenames = [f for f in os.listdir("./") if f.endswith("smoothed.fits") and f.startswith("H")]
+  corrdir = "Cross_correlations/"
+  vsini = "20"
+  Temperatures = [3300, 3500, 3700, 3900, 4200, 4500, 5000, 5500]
+  metals = ["+0.5", "-0.5"]
+  logg = "+4.0"
+  HelperFunctions.ensure_dir("Figures/")
+  for rootfile in filenames:
+    for T in Temperatures:
+      for metal in metals:
+        if T == 5000 and metal == "-0.5":
+          logg = "+3.5"
+        else:
+          logg = "+4.0"
+        corrfile = "%s%s.%skps_%iK%s%s" %(corrdir, 
+                                          rootfile.split(".fits")[0],
+                                          vsini,
+                                          T,
+                                          logg,
+                                          metal)
+        print rootfile, T, metal
+        vel, corr = numpy.loadtxt(corrfile, unpack=True)
+        plt.plot(vel, corr, 'k-', lw=2)
+        plt.xlabel("Velocity (km/s)")
+        plt.ylabel("CCF")
+        plt.title(r"%s:  $T_s$=%iK & [Fe/H]=%s" %(rootfile, T, metal))
+        plt.savefig("Figures/%s.pdf" %corrfile.split("/")[-1])
+        plt.grid()
+        plt.show()
+        plt.clf()
--- SensitivityAnalysis.py
+++ SensitivityAnalysis.py
@@ -30,9 +30,6 @@ def ensure_dir(f):
 homedir = os.environ["HOME"]
 modeldir = homedir + "/School/Research/Models/Sorted/Stellar/Vband/"
 
-#Define some constants to use
-vsini = 20*units.km.to(units.cm)
-resolution = 80000
 
 #Define regions contaminated by telluric residuals or other defects. We will not use those regions in the cross-correlation
 badregions = [[567.5, 575.5],
@@ -88,53 +85,6 @@ model_list = [ modeldir + "lte30-4.00-0.0.AGS.Cond.PHOENIX-ACES-2009.HighRes.7.s
                modeldir + "lte78-4.50-0.0.AGS.Cond.PHOENIX-ACES-2009.HighRes.7.sorted"]"""
    
               
-"""
-star_list = []
-temp_list = []
-gravity_list = []
-metal_list = []
-model_data = []
-start = 0
-end = len(model_list)
-fitorders = 8*numpy.ones(len(model_list))
-fitorders[0] = 7
-fitorders[1] = 6
-fitorders[5] = 7
-if __name__ == "__main__":
-  for i, fname in enumerate(model_list[start:end]):
-    if "PHOENIX2004" in fname:
-      temp = int(fname.split("lte")[-1][:2])*100
-      gravity = float(fname.split("lte")[-1][3:6])
-      metallicity = float(fname.split("lte")[-1][6:10])
-    elif "PHOENIX-ACES" in fname:
-      temp = int(fname.split("lte")[-1][:2])*100
-      gravity = float(fname.split("lte")[-1][3:7])
-      metallicity = float(fname.split("lte")[-1][7:11])
-    print "Reading in file %s" %fname
-    x,y = numpy.loadtxt(fname, usecols=(0,1), unpack=True)
-    #c = FittingUtilities.Continuum(x, y, fitorder=fitorders[i+start], lowreject=2, highreject=7)
-  
-    plt.plot(x,y)
-    for order in range(6,11):
-      print order
-      c = FittingUtilities.Continuum(x, y, fitorder=order, lowreject=2, highreject=7, numiter=10)
-      plt.plot(x,c, label="%ith order" %order)
-    plt.title(fname.split("/")[-1])
-    plt.legend(loc='best')
-    plt.show()
-    continue
-    model = DataStructures.xypoint(x=x*units.angstrom.to(units.nm)/1.00026, y=10**y, cont=10**c)
-    model = FittingUtilities.RebinData(model, numpy.linspace(model.x[0], model.x[-1], model.size()))
-    model = Broaden.RotBroad(model, vsini)
-    model = Broaden.ReduceResolution2(model, resolution)
-    model_data.append(interp(model.x, model.y/model.cont))
-    #model_data.append( model )
-    star_list.append(str(temp))
-    temp_list.append(temp)
-    gravity_list.append(gravity)
-    metal_list.append(metallicity)
-"""
-
 
 MS = SpectralTypeRelations.MainSequence()
 PMS = SpectralTypeRelations.PreMainSequence(pms_tracks_file="%s/Dropbox/School/Research/Stellar_Evolution/Baraffe_Tracks.dat" %(os.environ["HOME"]), track_source="Baraffe")
@@ -183,7 +133,7 @@ def GetMass(spt, age):
   T = MS.Interpolate(MS.Temperature, spt[:end])
 
   # Determine which tracks to use
-  if spt[0] == "O" or spt[0] == "A" or spt[0] == "F":
+  if spt[0] == "O" or spt[0] == "B" or spt[0] == "A" or spt[0] == "F":
     return PMS2.GetFromTemperature(age, T, key="Mass")
   else:
     return PMS.GetFromTemperature(age, T, key="Mass")
@@ -221,6 +171,9 @@ def GetAge(sptlist):
 
 
 if __name__ == "__main__":
+  #Define some constants to use
+  vsini_secondary = 20*units.km.to(units.cm)
+  resolution = 80000
   lightspeed = constants.c.cgs.value * units.cm.to(units.km)
   smooth_factor = 0.8
   vel_list = range(-400, 400, 50)
@@ -228,16 +181,20 @@ if __name__ == "__main__":
   vsini_file = "%s/School/Research/Useful_Datafiles/Vsini.csv" %(os.environ["HOME"])
   fileList = []
   tolerance = 5.0
+  debug = False
   for arg in sys.argv[1:]:
     if "-m" in arg:
       companion_file = arg.split("=")[1]
     elif "-tol" in arg:
       tolerance = float(arg.split("=")[1])
+    elif "-d" in arg:
+      debug = True
     else:
       fileList.append(arg)
 
   # Make sure each output file exists:
   logfilenames = {}
+  output_directories = {}
   for fname in fileList:
     output_dir = "Sensitivity/"
     outfilebase = fname.split(".fits")[0]
@@ -249,6 +206,7 @@ if __name__ == "__main__":
         output_dir = output_dir + directory + "/"
       output_dir = output_dir + "Sensitivity/"
     HelperFunctions.ensure_dir(output_dir)
+    output_directories[fname] = output_dir
     
     #Make the summary file
     logfile = open(output_dir + "logfile.dat", "w")
@@ -264,32 +222,10 @@ if __name__ == "__main__":
   vsini_data = ascii.read(vsini_file)[10:]
 
   # Now, start loop over the models:
-  start = 0
-  end = len(model_list)
-  fitorders = 8*numpy.ones(len(model_list))
+  firstmodel = 0
+  lastmodel = len(model_list)
   fitorders = [7, 6, 8, 8, 8, 7, 7, 10, 9, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 8, 8, 8, 8]
-  """
-  fitorders[0] = 7
-  fitorders[1] = 6
-  fitorders[5] = 7
-  fitorders[6] = 7
-  fitorders[7] = 10
-  fitorders[8] = 9
-  fitorders[9] = 7
-  fitorders[10] = 7
-  fitorders[11] = 7
-  fitorders[12] = 7
-  fitorders[13] = 7
-  fitorders[14] = 7
-  fitorders[15] = 7
-  fitorders[16] = 6
-  fitorders[17] = 6
-  fitorders[18] = 6
-  fitorders[19] = 6
-  """
-  #start = 15
-  #end = 16
-  for i, modelfile in enumerate(model_list[start:end]):
+  for modelnum, modelfile in enumerate(model_list[firstmodel:lastmodel]):
     if "PHOENIX2004" in modelfile:
       temp = int(modelfile.split("lte")[-1][:2])*100
       gravity = float(modelfile.split("lte")[-1][3:6])
@@ -300,21 +236,10 @@ if __name__ == "__main__":
       metallicity = float(modelfile.split("lte")[-1][7:11])
     print "Reading in file %s" %modelfile
     x,y = numpy.loadtxt(modelfile, usecols=(0,1), unpack=True)
-    c = FittingUtilities.Continuum(x, y, fitorder=fitorders[i+start], lowreject=2, highreject=7)
-    plt.plot(x,y)
-    plt.plot(x,c)
-    #for order in range(6,11):
-    #  print order
-    #  c = FittingUtilities.Continuum(x, y, fitorder=order, lowreject=2, highreject=7, numiter=10)
-    #  plt.plot(x,c, label="%ith order" %order)
-    plt.title(modelfile.split("/")[-1])
-    #plt.legend(loc='best')
-    plt.title(str(temp))
-    plt.show()
-    continue
+    c = FittingUtilities.Continuum(x, y, fitorder=int(fitorders[modelnum+firstmodel]), lowreject=2, highreject=7)
     model = DataStructures.xypoint(x=x*units.angstrom.to(units.nm)/1.00026, y=10**y, cont=10**c)
     model = FittingUtilities.RebinData(model, numpy.linspace(model.x[0], model.x[-1], model.size()))
-    model = Broaden.RotBroad(model, vsini)
+    model = Broaden.RotBroad(model, vsini_secondary)
     model = Broaden.ReduceResolution2(model, resolution)
     modelfcn = interp(model.x, model.y/model.cont)
     
@@ -322,10 +247,12 @@ if __name__ == "__main__":
     # Now that we have a spline function for the broadened data,
     # begin looping over the files
     for fname in fileList:
+      output_dir = output_directories[fname]
       outfile = open(logfilenames[fname], "a")
 
       # Read in and process the data like I am about to look for a companion
       orders_original = Process_Data(fname)
+      
 
       #Find the vsini of the primary star with my spreadsheet
       starname = pyfits.getheader(fname)["object"]
@@ -356,7 +283,6 @@ if __name__ == "__main__":
       #Determine the age of the system and properties of the primary and secondary star
       age = GetAge(known_stars)
       primary_spt = known_stars[0]
-      print primary_spt
       end = search("[0-9]", primary_spt).end()
       primary_temp = MS.Interpolate(MS.Temperature, primary_spt[:end])
       primary_mass = GetMass(primary_spt, age)
@@ -369,11 +295,9 @@ if __name__ == "__main__":
         orders = [order.copy() for order in orders_original]   #Make a copy of orders
         model_orders = []
         for ordernum, order in enumerate(orders):
-          #Ensure the x-axis
-          #xgrid = numpy.linspace(order.x[0], order.x[-1], order.size())
-          #order = FittingUtilities.RebinData(order, xgrid)
+          #Get the flux ratio
           scale = GetFluxRatio(known_stars, temp, order.x)
-          print ordernum, scale.mean()
+          print "Scale factor for order %i is %.3g" %(ordernum, scale.mean())
 
           #Add the model to the data
           model = (modelfcn(order.x*(1.0+rv/lightspeed)) - 1.0) * scale
@@ -388,7 +312,8 @@ if __name__ == "__main__":
                                        lowreject=3, 
                                        highreject=3,
                                        expand=10, 
-                                       numiters=10)
+                                       numiters=10,
+                                       normalize=False)
           order.y /= smoothed.y
 
           # log-space the data
@@ -409,15 +334,10 @@ if __name__ == "__main__":
 
           # Save model order
           model_orders.append(model)
+          orders[ordernum] = order.copy()
 
         #Do the actual cross-correlation
-        corr = Correlate.Correlate(orders, model_orders)#, debug=True, outputdir="Sensitivity_Testing/")
-
-        plt.figure(int(rv/50+ 10))
-        plt.plot(corr.x, corr.y)
-        plt.xlabel("Velocity (km/s)")
-        plt.ylabel("CCF")
-        plt.title("RV = %g" %rv)
+        corr = Correlate.Correlate(orders, model_orders, debug=debug, outputdir="Sensitivity_Testing/")
 
         # Check if we found the companion
         idx = numpy.argmax(corr.y)
@@ -428,6 +348,10 @@ if __name__ == "__main__":
         mean = corr.y[goodindices].mean()
         std = corr.y[goodindices].std()
         significance = (corr.y[idx] - mean)/std
+        if debug:
+          corrfile = "%s%s_t%i_v%i" %(output_dir, fname.split("/")[-1].split(".fits")[0], temp, rv)
+          print "Outputting CCF to %s" %corrfile
+          numpy.savetxt(corrfile, numpy.transpose((corr.x, corr.y-mean, numpy.ones(corr.size())*std)), fmt="%.10g")
         if abs(vmax - rv) <= tolerance:
           #Found
           outfile.write("%s\t%i\t\t\t%i\t\t\t\t%.2f\t\t%.4f\t\t%i\t\tyes\t\t%.2f\n" %(fname, primary_temp, temp, secondary_mass, massratio, rv, significance) )
@@ -437,117 +361,5 @@ if __name__ == "__main__":
         
       outfile.close()
 
-
-
-  
-  plt.show()
-  
-
-"""
-if __name__ == "__main__":
-  #Parse command line arguments:
-  fileList = []
-  extensions=True
-  tellurics=False
-  trimsize = 1
-  windowsize = 101
-  vsini = 10.0*units.km.to(units.cm)
-  MS = SpectralTypeRelations.MainSequence()
-  PMS = SpectralTypeRelations.PreMainSequence()
-  vel_list = range(-400, 400, 50)
-  outdir = "Sensitivity/"
-  for arg in sys.argv[1:]:
-    if "-e" in arg:
-      extensions=False
-    if "-t" in arg:
-      tellurics=True  #telluric lines modeled but not removed
-    else:
-      fileList.append(arg)
-
-  ensure_dir(outdir)
-  outfile = open(outdir + "logfile.dat", "w")
-  outfile.write("Sensitivity Analysis:\n*****************************\n\n")
-  outfile.write("Filename\t\t\tPrimary Temperature\tSecondary Temperature\tMass (Msun)\tMass Ratio\tVelocity\tPeak Correct?\tSignificance\n")
-  
-  for fname in fileList:
-    orders_original = ProcessData(fname)
-    
-    #Read in the name of the star from the fits header
-    header = pyfits.getheader(fname)
-    starname = header["OBJECT"]
-    print starname
-
-    #Get spectral type of the primary from the name and simbad
-    stardata = StarData.GetData(starname)
-    primary_temp = [ MS.Interpolate(MS.Temperature, stardata.spectype[:2]), ]
-    #age = 'MS'   #Play with this later!
-    primary_mass = MS.Interpolate(MS.Mass, stardata.spectype[:2])
-    age = PMS.GetMainSequenceAge(primary_mass)
-    
-
-    #Check for close companions
-    companions = HelperFunctions.CheckMultiplicityWDS(starname)
-    if companions:
-      for configuration in companions:
-        component = companions[configuration]
-        if component["Separation"] < 3.0 and component["Secondary SpT"] != "Unknown":
-          print "Known %s companion with a separation of %g arcseconds!" %(component["Secondary SpT"], component["Separation"])
-          primary_temp.append(MS.Interpolate(MS.Temperature, component["Secondary SpT"]))
-    companion = HelperFunctions.CheckMultiplicitySB9(starname)
-    if companion:
-      if companion['K1'] != 'Unknown' and companion['K2'] != 'Unknown':
-        mass = primary_mass * companion['K1'] / companion['K2']
-        spt = MS.GetSpectralType(MS.Mass, mass, interpolate=True)
-        primary_temp.append(MS.Interpolate(MS.Temperature, spt))
-          
-
-    #Begin loop over model spectra
-    for j, model in enumerate(model_data):
-            
-      #Get info about the secondary star for this model temperature
-      secondary_spt = MS.GetSpectralType(MS.Temperature, temp_list[j])
-      secondary_radius = PMS.Interpolate(secondary_spt, age, key='Radius')
-      secondary_mass = PMS.Interpolate(secondary_spt, age, key='Mass')
-      massratio = secondary_mass / primary_mass
-
-      #Rotationally Broaden model
-      left = numpy.searchsorted(model.x, orders_original[0].x[0] - 10.0)
-      right = numpy.searchsorted(model.x, orders_original[-1].x[-1] + 10.0)
-      model = Broaden.RotBroad(model[left:right], vsini, linear=True)
-
-      #Reduce resolution
-      model = FittingUtilities.ReduceResolution2(model, 60000, extend=False)
-
-      #Check sensitivity to this star
-      orders = [order.copy() for order in orders_original]   #Make a copy of orders
-      output_dir = "%s/Sensitivity/" %(os.path.dirname(fname))
-      if output_dir.startswith("/"):
-        output_dir = output_dir[1:]
-      found, significance = Sensitivity.Analyze(orders, 
-                                                model, 
-                                                prim_temp=primary_temp, 
-                                                sec_temp=temp_list[j],
-                                                age=age,
-                                                smoothing_windowsize=101,
-                                                smoothing_order=5,
-                                                outdir=output_dir,
-                                                outfilebase=fname.split(".fits")[0],
-                                                debug=False)
-
-      #Write to logfile
-      vels=range(-400, 450, 50)
-      for v,f,s in zip(vels, found, significance):
-        if f:
-          #Signal found!
-          outfile.write("%s\t%i\t\t\t%i\t\t\t\t%.2f\t\t%.4f\t\t%i\t\tyes\t\t%.2f\n" %(fname, primary_temp[0], temp_list[j], secondary_mass, massratio, v, s) )
-        else:
-          outfile.write("%s\t%i\t\t\t%i\t\t\t\t%.2f\t\t%.4f\t\t%i\t\tno\t\tN/A\n" %(fname, primary_temp[0], temp_list[j], secondary_mass, massratio, v) )
-      
-       
-
-          
-  outfile.close()
-"""      
-    
 
 
