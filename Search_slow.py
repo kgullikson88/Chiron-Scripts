4a41111ec0387c28b240cec9e496e15d3e09e036
kgullikson88 <kevin.gullikson@gmail.com>
2015-02-08 14:20:28 -0600

    Settings update

--- Search_slow.py
+++ Search_slow.py
@@ -70,5 +70,5 @@ if __name__ == '__main__':
                                         debug=False,
                                         vbary_correct=False,
                                         addmode='simple',
-                                        output_mode='hdf')
+                                        output_mode='hdf5')
 
